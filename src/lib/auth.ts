c384a6af8973571b247f4d833bb92017c0448930
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-09-26 12:03:04 +0530

    added db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "./db";
+import { Keypair } from "@solana/web3.js";
+import { Account, Profile, User } from "next-auth";
 
 export const authOptions = {
   providers: [
@@ -7,5 +10,52 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account }: { user: User; account: Account }) {
+      if (account.provider === "google") {
+        const email = user.email;
+        if (!email) {
+          return false;
+        }
+
+        const isUserExist = await prisma.user.findFirst({
+          where: {
+            email: user?.email as string,
+          },
+        });
+
+        if (isUserExist) {
+          return true;
+        }
+
+        const keyPair = Keypair.generate();
+        const publicKey = keyPair.publicKey.toBase58();
+        const privateKey = keyPair.secretKey.toString();
+
+        await prisma.user.create({
+          data: {
+            name: user.name as string,
+            provider: "Google",
+            image: user.image as string,
+            email: email,
+            solWallet: {
+              create: {
+                privateKey,
+                publicKey,
+              },
+            },
+            inrWallet: {
+              create: {
+                balance: 0,
+              },
+            },
+          },
+        });
+
+        return true;
+      }
+      return false;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
